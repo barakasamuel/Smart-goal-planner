b047c4debf489f3a9254f292706de5ff244db6f3
Samuel Baraka <barakasamuel125@gmail.com>
2025-07-24 18:08:54 +0300

    Updates vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smart-planner/',
+  base: './smart-goal-planner/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
